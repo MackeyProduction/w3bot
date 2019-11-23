e1cd46db23ca75c640360f4d1dcee54d61b2b129
Til Anheier <tmtipps-help@gmx.de>
2019-11-24 00:53:37 +0100

    - post and put method added.

--- w3bot/Database/Repository/AbstractHttpRepository.cs
+++ w3bot/Database/Repository/AbstractHttpRepository.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,8 @@ namespace w3bot.Database.Repository
     abstract class AbstractHttpRepository
     {
         private HttpClient _httpClient;
+        private Dictionary<string, string> headers = new Dictionary<string, string>();
+
         protected const string ENDPOINT = "http://127.0.0.1:8000/api";
 
         protected bool IsReady { get; }
@@ -20,12 +24,57 @@ namespace w3bot.Database.Repository
             _httpClient = httpClient;
         }
 
+        protected void AddHeader(string headerType, string headerValue)
+        {
+            headers.Add(headerType, headerValue);
+        }
+
+        protected void RemoveHeader(string headerType)
+        {
+            headers.Remove(headerType);
+        }
+
         protected async Task<HttpResponseMessage> Fetch(string endpoint)
         {
+            return await SendRequest(endpoint, HttpMethod.Get);
+        }
+
+        protected async Task<HttpResponseMessage> Post(string endpoint, Dictionary<string, string> content)
+        {
+            return await SendRequest(endpoint, HttpMethod.Post, content);
+        }
+
+        protected async Task<HttpResponseMessage> Put(string endpoint, Dictionary<string, string> content)
+        {
+            return await SendRequest(endpoint, HttpMethod.Put, content);
+        }
+
+        protected async Task<HttpResponseMessage> SendRequest(string endpoint, HttpMethod method, Dictionary<string, string> content = null)
+        {
+            // set application json header for request
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
             HttpResponseMessage result;
             try
             {
-                result = await _httpClient.GetAsync($"{endpoint}");
+                var request = new HttpRequestMessage(method, $"{endpoint}");
+
+                // send data with content
+                if (content != null)
+                {
+                    var encodedContent = new FormUrlEncodedContent(content);
+                    request.Content = encodedContent;
+                }
+
+                if (headers.Count > 0)
+                {
+                    foreach (KeyValuePair<string, string> dictionary in headers)
+                    {
+                        request.Headers.Add(dictionary.Key, dictionary.Value);
+                    }
+                }
+
+                result = await _httpClient.SendAsync(request);
             }
             catch (Exception exception)
             {
@@ -33,16 +82,6 @@ namespace w3bot.Database.Repository
             }
 
             return result;
-        }
-
-        protected async Task<HttpResponseMessage> Post(string endpoint, Dictionary<string, string> data)
-        {
-            return null;
-        }
-
-        protected async Task<HttpResponseMessage> Pull(string endpoint)
-        {
-            return null;
         }
 
         protected async Task<string> HttpContentAsString(HttpContent content)
